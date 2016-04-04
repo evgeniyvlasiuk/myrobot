8a2b8eebe163efbebfe069d6ffb3c85a0050c07a
evgeniyvlasiuk <vlasiuk.zp@gmail.com>
2016-04-04 22:53:14 +0300

    myrobot

--- robot.js
+++ robot.js
@@ -1,24 +1,45 @@
 $(document).ready(function() {
-    $(document).keydown(function(key) {
-        
-      
-       switch(parseInt(key.which,10)) {
-        
-			case 37:
-				$('img').animate({left: "-=40px"}, 'fast');
-				break;
+var bob = $(this).attr('id');
+$(document).keydown(function(key) {
 
-			case 38:
-				$('img').animate({top: "-=40px"}, 'fast');
-				break;
-			case 39:
-				$('img').animate({left: "+=40px"}, 'fast');
-				break;
-			case 40:
-				$('img').animate({top: "+=40px"}, 'fast');
-				break;
-        }
-        
-	});
+var t = parseInt(window.getComputedStyle($("#bob")[0]).top);
+var l = parseInt(window.getComputedStyle($("#bob")[0]).left);
 
+switch(parseInt(key.which,10)) {
+
+case 37:
+ if (l <= 0) {
+  $('img').animate({left: "=0px"}, 'fast');
+ }
+ else {
+  $('img').animate({left: "-=40px"}, 'fast');
+ }
+break;
+case 38:
+ if (t <= 0) {
+  $('img').animate({top: "=0px"}, 'fast');
+ }
+ else {
+  $('img').animate({top: "-=40px"}, 'fast');
+ }
+break;
+case 39:
+ if (l >= 720){
+  $('img').animate({left: "=720px"}, 'fast');
+ }
+ else {
+  $('img').animate({left: "+=40px"}, 'fast');
+ }
+break;
+case 40:
+ if (t >= 520) {
+  $('img').animate({top: "=520px"}, 'fast');
+ }
+ else{
+  $('img').animate({top: "+=40px"}, 'fast');
+ }
+break;
+}
+
+});
 });
